45aa44c00432ba31063acf302c66a050a67a6746
Jorge Fco <jorge_frac@hotmail.com>
2017-11-12 22:31:27 -0600

    Init sizes for variables

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
+### Variables
+* xs
+* small
+* medium
+* large
+* full
+
+```
+$screen-xs:				480px  !default;		// -> Extra small (Phone)
+$screen-sm:			768px  !default;		// -> Small  (Smartphone)
+$screen-md:			1024px  !default;		// -> Medium (Laptop and tablets)
+$screen-lg:				1224px  !default;		// -> Large (Desktops PC)
+$screen-full:			1824px  !default;		// -> Full (Device full HD)
+```
+
+
 Install bower
 
 ```
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<title>Device</title>
+
+	<!--CSS-->
+	<link rel="stylesheet" type="text/css" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<header class="jumbotron">
+		<div class="container">
+			<h1>Devices</h1>
+		</div>
+	</header>
+
+	<section class="">
+		<h1></h1>
+	</section>
+
+	<footer>
+		<div class="container">
+			
+		</div>
+	</footer>
+</body>
+</html>
